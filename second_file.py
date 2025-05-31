5edf6ffcbee1fd95c0e328e2fab8d5573ba2bf2e
Akshaypr <akshaypr314159@gmail.com>
2025-05-31 15:25:03 +0530

    Added name parameter

--- second_file.py
+++ second_file.py
@@ -2,7 +2,7 @@ def print_hello_world():
     print("Hello World")
     print("Hello print world!")
 
-def print_my_name():
-    print("My name is Akshay")
+def print_my_name(name):
+    print(name)
 
 print_hello_world()    
